d79355690b7e9a1c7c1c70d96408390a691ade8a
Jakub Gorczynski <jakub.gorczynski00@gmail.com>
2020-10-17 21:04:24 +0200

    Add project files.

---
+++ StockServerApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft.Json;
+using RestSharp.Authenticators;
+
+namespace StockClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("https://stockserver20201009223011.azurewebsites.net/");
+            client.Authenticator = new HttpBasicAuthenticator("01149601@pw.edu.pl", "mA4YBUw");
+            
+            var stockListRequest = new RestRequest("stockexchanges", DataFormat.Json);
+            var stockListJSONResponse = client.Get(stockListRequest);
+
+            var sharesListRequest = new RestRequest("shareslist/Londyn");
+            var sharesListJSONResponse = client.Get(sharesListRequest);
+
+            string[] stockExchanges = JsonConvert.DeserializeObject<string[]>(stockListJSONResponse.Content);
+            string[] sharesList = JsonConvert.DeserializeObject<string[]>(sharesListJSONResponse.Content);
+
+            foreach (string stockExchange in stockExchanges)
+                Console.WriteLine(stockExchange);
+
+            Console.WriteLine("\nShares to buy or sell in London:");
+
+            foreach (string share in sharesList)
+                Console.WriteLine(share);
+
+            Console.ReadKey();
+        }
+    }
+}
